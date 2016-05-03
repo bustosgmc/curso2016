7f06096bba234e695cca0b6b5daf2f9277a2c07c
Jose Luis Llorente <Jose Luis Llorente@192.168.247.128>
2016-05-03 12:43:36 +0200

    prueba mi usuario

--- TestGit/src/es/curso/integracioncontinua/HolaMundo.java
+++ TestGit/src/es/curso/integracioncontinua/HolaMundo.java
@@ -4,7 +4,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hola Mundo Integracion Continua 3");
+		System.out.println("Hola Mundo Integracion Continua mio");
 
 	}
 
