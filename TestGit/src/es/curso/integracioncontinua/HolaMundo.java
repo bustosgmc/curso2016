e3ccee090a0d5bbafd94acf9f2817c2f47d0b775
Jose Luis Llorente <Jose Luis Llorente@192.168.247.128>
2016-05-03 11:00:01 +0200

    Mensaje primer commit

---
+++ TestGit/src/es/curso/integracioncontinua/HolaMundo.java
@@ -0,0 +1,11 @@
+package es.curso.integracioncontinua;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hola Mundo Integracion Continua");
+
+	}
+
+}
